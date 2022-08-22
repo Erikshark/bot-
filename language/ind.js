847526832e387abddcaa684374ac523bb6919382
Erikshark <103891869+Erikshark@users.noreply.github.com>
2022-08-22 08:27:49 -0500

    Add files via upload

---
+++ language/ind.js
@@ -0,0 +1,182 @@
+const a = '```'
+
+exports.wait = () => {
+	return`*「 𝑬𝑺𝑷𝑬𝑹𝑨 」*`
+}
+
+exports.succes = () => {
+	return`*「 𝑳𝑰𝑺𝑻𝑶 」*`
+}
+
+exports.lvlon = () => {
+	return`*「 𝑨𝑪𝑻𝑰𝑽𝑨𝑫𝑶 」 𝐿𝐸𝑉𝐸𝐿𝐼𝑁𝐺*`
+}
+
+exports.lvloff = () => {
+	return`*「 𝑫𝑬𝑺𝑨𝑪𝑻𝑰𝑽𝑨𝑫𝑶 」 𝐿𝐸𝑉𝐸𝐿𝐼𝑁𝐺*`
+}
+
+exports.lvlnul = () => {
+	return`*𝑇𝑈 𝑁𝐼𝑉𝐸𝐿 𝐴𝑈𝑁 𝐸𝑆𝑇𝐴 𝑉𝐴𝐶𝐼𝑂*`
+}
+
+exports.lvlnoon = () => {
+	return`*𝐸𝐿 𝑁𝐼𝑉𝐸𝐿 𝑇𝑂𝐷𝐴𝑉𝐼𝐴 𝑁𝑂 𝐸𝑆𝑇𝐴 𝐴𝐶𝑇𝐼𝑉𝑂*`
+}
+
+exports.noregis = () => {
+	return`*「 𝑵𝑶 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑨𝑫𝑶 」*\n\n*𝑃𝑎𝑟𝑎 𝑟𝑒𝑔𝑖𝑠𝑡𝑟𝑎𝑟𝑠𝑒 𝑑𝑒𝑏𝑒 𝑒𝑠𝑐𝑟𝑖𝑏𝑖𝑟 : ${prefix}registrar nombre|edad* \n*𝐸𝑗𝑒𝑚𝑝𝑙𝑜: ${prefix}registrar Confu|22*`
+}
+
+exports.rediregis = () => {
+	return`*「 𝒀𝑨 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑨𝑫𝑶 」*\n\n*𝑌𝑎 𝑒𝑠𝑡𝑎𝑠 𝑟𝑒𝑔𝑖𝑠𝑡𝑟𝑎𝑑𝑜 𝑒𝑛 𝑙𝑎 𝑏𝑎𝑠𝑒 𝑑𝑒 𝑑𝑎𝑡𝑜𝑠*`
+}
+
+exports.stikga = () => {
+	return`*𝑆𝑖 𝑓𝑎𝑙𝑙𝑜 𝑖𝑛𝑡𝑒𝑛𝑡𝑒 𝑛𝑢𝑒𝑣𝑎𝑚𝑒𝑛𝑡𝑒 𝑒𝑛 𝑢𝑛 𝑚𝑜𝑚𝑒𝑛𝑡𝑜*`
+}
+
+exports.linkga = () => {
+	return`*𝐿𝑖𝑛𝑘 𝑖𝑛𝑣𝑎𝑙𝑖𝑑𝑜*`
+}
+
+exports.groupo = () => {
+	return`*「 𝑺𝑶𝑳𝑶 𝑳𝑶 𝑷𝑼𝑬𝑫𝑬𝑺 𝑼𝑺𝑨𝑹 𝑬𝑵 𝑮𝑹𝑼𝑷𝑶𝑺 」*`
+}
+
+exports.ownerb = () => {
+	return`*「 𝑺𝑶𝑳𝑶 𝑳𝑶 𝑷𝑼𝑬𝑫𝑬 𝑼𝑺𝑨𝑹 𝑴𝑰 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 」*`
+}
+
+exports.ownerg = () => {
+	return`*「 𝑺𝑶𝑳𝑶 𝑳𝑶 𝑷𝑼𝑬𝑫𝑬 𝑼𝑺𝑨𝑹 𝑬𝑳 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 𝑫𝑬𝑳 𝑮𝑹𝑼𝑷𝑶 」*`
+}
+
+exports.admin = () => {
+	return`*「 𝑺𝑶𝑳𝑶 𝑳𝑶 𝑷𝑼𝑬𝑫𝑬 𝑼𝑺𝑨𝑹 𝑳𝑶𝑺 𝑨𝑫𝑴𝑰𝑵𝑺 𝑫𝑬𝑳 𝑮𝑹𝑼𝑷𝑶 」*`
+}
+
+exports.badmin = () => {
+	return`*「 𝑩𝑶𝑻 𝑫𝑬𝑩𝑬 𝑺𝑬𝑹 𝑨𝑫𝑴𝑰𝑵 」*`
+}
+
+exports.nsfwoff = () => {
+	return`*𝑵𝑰𝑮𝑼𝑵 𝑨𝑫𝑵𝑰𝑴 𝑨𝑯 𝑨𝑪𝑻𝑰𝑽𝑨𝑫𝑶 𝑬𝑳 𝑵𝑺𝑭𝑾*`
+}
+
+exports.bug = () => {
+	return`*𝑆𝐸𝐿𝐸 𝐴 𝐼𝑀𝐹𝑂𝑅𝑀𝐴𝐷𝑂 𝐴𝐿 𝐶𝑅𝐸𝐴𝐷𝑂𝑅 𝑆𝑂𝐵𝑅𝐸 𝐿𝑂𝑆 𝑃𝑅𝑂𝐵𝐿𝐸𝑀𝐴𝑆*`
+}
+
+exports.wrongf = () => {
+	return`*𝐹𝑂𝑅𝑀𝐴𝑇𝑂 𝐼𝑁𝐶𝑂𝑅𝑅𝐸𝐶𝑇𝑂 / 𝑇𝐸𝑋𝑇𝑂 𝐸𝑁 𝐵𝐿𝐴𝑁𝐶𝑂*`
+}
+
+exports.clears = () => {
+	return`*𝐵𝑂𝑅𝑅𝐴𝑅 𝑇𝑂𝐷𝑂 𝐸𝑋𝐼𝑇𝑂𝑆𝑂*`
+}
+
+exports.pc = () => {
+	return`*「 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑶 」*\n\n𝑃𝑎𝑟𝑎 𝑠𝑎𝑏𝑒𝑟 𝑠𝑖 𝑠𝑒 𝑎 𝑟𝑒𝑔𝑢𝑠𝑡𝑟𝑜, 𝑒𝑛𝑣𝑖𝑒 𝑒𝑙 𝑚𝑒𝑛𝑠𝑎𝑗𝑒 𝑞𝑢𝑒 𝑙𝑒 𝑒𝑛𝑣𝑖𝑒\n\n𝑵𝑶𝑻𝑨:\n*𝑆𝑖 𝑛𝑜 𝑎 𝑟𝑒𝑐𝑖𝑏𝑖𝑑𝑜 𝑢𝑛 𝑚𝑒𝑛𝑠𝑎𝑗𝑒 𝑠𝑖𝑛𝑖𝑓𝑖𝑐𝑎 𝑞𝑢𝑒 𝑛𝑜 𝑠𝑒 𝑎 𝑔𝑢𝑎𝑟𝑑𝑎𝑑𝑜 𝑠𝑢 𝑛𝑢𝑚𝑒𝑟𝑜 *`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`*「 𝑫𝑨𝑻𝑶𝑺 」*\n\n𝐹𝑒𝑙𝑒𝑐𝑖𝑡𝑎𝑐𝑖𝑜𝑛𝑒𝑠 𝑡𝑒 ℎ𝑎𝑠 𝑟𝑒𝑔𝑖𝑠𝑡𝑟𝑎𝑑𝑜 \n\n┏━⊱𝑁𝑜𝑚𝑏𝑟𝑒\n┗⊱${namaUser}\n┏━⊱𝑁𝑢𝑚𝑒𝑟𝑜\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱𝐸𝑑𝑎𝑑\n┗⊱${umurUser}\n┏━⊱𝐻𝑜𝑟𝑎\n┗⊱${time}\n\n┏━❉ *𝑆𝑁* ❉━\n┣⊱${serialUser}\n┗⊱𝑵𝑶𝑻𝑨 : 𝑁𝑜 𝑠𝑝𝑎𝑚:v`
+}
+
+exports.cmdnf = (prefix, command) => {
+	return`𝐶𝑜𝑚𝑎𝑛𝑑𝑜 *${prefix}${command}* 𝑁𝑜 𝑟𝑒𝑔𝑖𝑠𝑡𝑟𝑎𝑑𝑜 𝑒𝑛 𝑚𝑖 𝑏𝑎𝑠𝑒 𝑑𝑒 𝑑𝑎𝑡𝑜𝑠 𝑒𝑠𝑐𝑟𝑖𝑏𝑒* ${prefix}menu*`
+}
+
+exports.owneresce = (pushname) => {
+	return`*𝑃𝑒𝑟𝑑𝑜𝑛 𝑝𝑒𝑟𝑜 ${pushname} 𝑛𝑜 𝑒𝑠 𝑒𝑙 𝑠𝑐𝑟𝑖𝑝 𝑑𝑒𝑙 𝑝𝑟𝑜𝑝𝑖𝑒𝑡𝑎𝑟𝑖𝑜*`
+}
+
+exports.reglevelaha = (command, pushname, getLevetariongLevel, sender, aha) => {
+	return`*𝐿𝑜 𝑠𝑖𝑒𝑛𝑡𝑜 ${pushname} 𝑡𝑢 𝑛𝑖𝑣𝑒𝑙 𝑛𝑜 𝑒𝑠 𝑠𝑢𝑓𝑖𝑐𝑖𝑒𝑛𝑡𝑒*\n\n*┏⊱𝑇𝑢 𝑛𝑖𝑣𝑒𝑙 : ${getLevelingLevel(sender)}*\n*┣⊱𝑇𝑖𝑝𝑜 𝑑𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 : ${command}*\n*┗⊱𝑅𝑒𝑞𝑢𝑖𝑠𝑖𝑡𝑜𝑠 𝑑𝑒 𝑛𝑖𝑣𝑒𝑙 : ${aha}*`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Lo siento ${pushname} tu level no es suficiente*\n\n*┏⊱tu level : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nivel : ${ahb}*`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Lo siento ${pushname} tu level no es suficiente*\n\n*┏⊱tu level : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nivel : ${ahc}*`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Lo siento ${pushname} tu level no es suficiente*\n\n*┏⊱tu level : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nivel : ${ahd}*`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Lo siento ${pushname} tu level no es suficiente*\n\n*┏⊱tu level : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nivel : ${ahe}*`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*𝐿𝑜 𝑠𝑖𝑒𝑛𝑡𝑜 ${pushname} 𝑡𝑢 𝑛𝑖𝑣𝑒𝑙 𝑛𝑜 𝑒𝑠 𝑠𝑢𝑓𝑖𝑐𝑖𝑒𝑛𝑡𝑒*\n\n*┏⊱𝑇𝑢 𝑛𝑖𝑣𝑒𝑙l : ${getLevelingLevel(sender)}*\n*┣⊱𝑇𝑖𝑝𝑜 𝑑𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 : ${command}*\n*┗⊱𝑅𝑒𝑞𝑢𝑖𝑠𝑖𝑡𝑜𝑠 𝑑𝑒 𝑛𝑖𝑣𝑒𝑙 : ${ahf}*`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
+	return`
+	
+*「 𝑵𝑰𝑽𝑬𝑳! 」*
+┏⊱ *𝑁𝑜𝑚𝑏𝑟𝑒* : ${pushname}
+┣⊱ *𝑁𝑢𝑚𝑒𝑟𝑜* : wa.me/${sender.split("@")[0]}
+┣⊱ *𝑋𝑝* : ${getLevelingXp(sender)}
+┣⊱ *𝐶𝑜𝑖𝑛𝑠* : +20
+┣⊱ *𝑅𝑜𝑙*: ${role}
+┗⊱ *𝑁𝑖𝑣𝑒𝑙* : ${getLevel} ⊱ ${getLevelingLevel(sender)}
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*𝐿𝑜 𝑠𝑖𝑒𝑛𝑡𝑜 ${pushname} 𝑡𝑢𝑠 𝑙𝑖𝑚𝑖𝑡𝑒𝑠 𝑑𝑒 ℎ𝑜𝑦 𝑠𝑒 𝑎𝑔𝑜𝑡𝑎𝑟𝑜𝑛*\n*𝑝𝑢𝑒𝑑𝑒𝑠 𝑐𝑜𝑚𝑝𝑟𝑎𝑟 𝑙𝑖𝑚𝑖𝑡𝑒𝑠 / 𝑠𝑢𝑏𝑖𝑟 𝑑𝑒 𝑛𝑖𝑣𝑒𝑙*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`
+*「 𝑻𝑼𝑺 𝑾𝑨𝑰𝑭𝑼𝑺 」*
+𝐴𝑠 𝑜𝑏𝑡𝑒𝑛𝑖𝑑𝑜 : ${limitCounts} 𝑊𝑖𝑓𝑢𝑠`
+}
+
+exports.satukos = () => {
+	return`*𝐴𝑔𝑟𝑒𝑔𝑎𝑟 𝑝𝑎𝑟𝑎𝑚𝑒𝑡𝑟𝑜 1 / ℎ𝑎𝑏𝑖𝑙𝑖𝑡𝑎𝑟 𝑜 0 / 𝑑𝑒𝑠𝑎𝑏𝑖𝑙𝑖𝑡𝑎𝑟`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`┏⊱ 「 *CARTERA* 」⊰━┓\n┣⊱ *Nombre* : ${pushname}\n┣⊱ *Numero* : ${sender.split("@")[0]}\n┣⊱ *Dinero* : ${uangkau}\n┗━━━━━━━━━━`
+}
+
+exports.afkOn = (pushname, reason) => {
+    return `𝐿𝑎 𝑓𝑢𝑛𝑐𝑖𝑜𝑛 𝐴𝐹𝐾 𝑓𝑢 𝑒𝑥𝑖𝑡𝑜𝑠𝑎 *𝐴𝐶𝑇𝐼𝑉𝐴𝐷𝐴*!\n\n➸ *𝑈𝑠𝑢𝑎𝑟𝑖𝑜*: ${pushname}\n➸ *𝑀𝑜𝑡𝑖𝑣𝑜*: ${reason}`
+}
+
+exports.afkOnAlready = () => {
+    return `𝐿𝑎 𝑓𝑢𝑛𝑐𝑖𝑜𝑛 𝐴𝐹𝐾 𝑠𝑒 𝑎𝑐𝑡𝑖𝑣𝑜 𝑑𝑒 𝑎𝑛𝑡𝑒𝑚𝑎𝑛𝑜.`
+}
+
+exports.afkMentioned = (getReason, getTime) => {
+    return `*.`
+}
+
+exports.afkDone = (pushname) => {
+    return `*${pushname}*`
+}
+
+exports.left = (num) => {
+	return`𝐹𝑖𝑛𝑎𝑙𝑚𝑒𝑛𝑡𝑒 𝑠𝑒 𝑟𝑒𝑑𝑢𝑐𝑒 𝑙𝑎 𝑐𝑎𝑟𝑔𝑎 𝑑𝑒𝑙 𝑔𝑟𝑢𝑝𝑜 𝑎 𝟭,𝑏𝑦𝑒 𝑏𝑦𝑒🥳 @${num.split('@')[0]} 𝑡𝑢𝑠 𝑠𝑒𝑟𝑣𝑖𝑐𝑖𝑜𝑠 𝑠𝑒𝑟𝑎𝑛 𝑒𝑛𝑡𝑒𝑟𝑎𝑑𝑜𝑠 𝑝𝑟𝑜𝑓𝑢𝑛𝑑𝑎𝑚𝑒𝑛𝑡𝑒²`
+}
+
+exports.welcome = ( mdata , num ) => {
+	return`@${num.split('@')[0]}
+𝐒𝐈 𝐄𝐑𝐄𝐒 𝐍𝐔𝐄𝐕𝐎 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐀𝐓𝐄 𝐎 𝐁𝐀𝐍 🧃
+
+𝐍𝐎𝐌𝐁𝐑𝐄 : 
+
+𝐅𝐎𝐓𝐎 : 
+
+𝐄𝐃𝐀𝐃 : 
+
+𝐏𝐀𝐈𝐒 : 
+
+𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 𝐀 𝐄𝐒𝐓𝐄 𝐆𝐑𝐔𝐏𝐎 𝐆𝐔𝐀𝐏@ 🍭
+`}
