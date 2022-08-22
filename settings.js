dc305701aa6e790c1ee90209d4388814b3fa359a
Erikshark <103891869+Erikshark@users.noreply.github.com>
2022-08-22 08:33:26 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,127 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Web\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'tu apikey',
+}
+
+global.xteam = 'RiyGanz'
+
+
+global.autoTyping = false 
+global.autoreadpmngc = false 
+global.autoReadGc = false 
+global.autoRecord = false
+global.available = true 
+
+
+global.vidmenu = fs.readFileSync("./Media/theme/bot.mp4") 
+
+
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.vcardowner = ['56999448163'] 
+global.ownername = "𝑴𝑰𝑺𝑨𝑬𝑳"
+global.ytname = "YT: " 
+global.socialm = "GitHub: " 
+global.location = "America, Lima, Perú" 
+
+//bot bomdy 
+global.owner = ['56999448163']
+global.ownertag = '56999448163' 
+global.botname = '𝑳𝑬𝑮𝑬𝑵𝑫𝑨𝑹𝒀-𝑩𝑶𝑻' 
+global.linkz = "https://youtube.com/c/ConfuMods" 
+global.websitex = "https://youtube.com/c/ConfuMods" 
+global.botscript = 'https://github.com/Confusion245/ConfuBot-MD5' 
+global.reactmoji = "✔️" 
+global.themeemoji = "🏳️‍🌈" 
+global.packname = "𝑺𝒕𝒊𝒄𝒌𝒆𝒓 \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.author = "𝑩𝒚 𝑳𝒆𝒈𝒆𝒏𝒅𝒂𝒓𝒚"
+
+
+global.thum = fs.readFileSync("./Media/theme/bot.jpg") 
+global.log0 = fs.readFileSync("./Media/theme/bot.jpg") 
+global.err4r = fs.readFileSync("./Media/theme/bot.jpg") 
+global.thumb = fs.readFileSync("./Media/theme/bot.jpg") 
+
+global.lolkeys = ['2e0da1f78d1721134b21816d', '902c3bc9d8c08b0dcf8f5373', '808693688ecc695293359089', '85faf717d0545d14074659ad']
+global.lolkeysapi = lolkeys[Math.floor(lolkeys.length * Math.random())]
+
+global.premium = ['56999448163'] 
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','#','/','?']
+global.sp = '⭔'
+global.mess = {
+    success: '𝐿𝑖𝑠𝑡𝑜 ✔️',
+    admin: '*[⚙️] 𝐸𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑒𝑠 𝑠𝑜𝑙𝑜 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑑𝑜 𝑝𝑜𝑟 𝑢𝑛 𝑎𝑑𝑚𝑖!*',
+    botAdmin: '*[⚙️] 𝐸𝑙 𝑏𝑜𝑡 𝑑𝑒𝑏𝑒 𝑠𝑒𝑟 𝑎𝑑𝑚𝑖𝑛 𝑝𝑎𝑟𝑎 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑟 𝑒𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜!*',
+    owner: '*[⚙️] 𝐸𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑠𝑜𝑙𝑜 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑑𝑜 𝑝𝑜𝑟 𝑒𝑙 𝑐𝑟𝑒𝑎𝑑𝑜𝑟 𝑑𝑒𝑙 𝑏𝑜𝑡!*',
+    group: '*[⚙️] 𝐸𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑠𝑜𝑙𝑜 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑑𝑜 𝑒𝑛 𝑔𝑟𝑢𝑝𝑜𝑠!*',
+    private: '*[⚙️] 𝐸𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑠𝑜𝑙𝑜 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑑𝑜 𝑒𝑛 𝑐ℎ𝑎𝑡 𝑝𝑟𝑖𝑣𝑎𝑑𝑜!*',
+    bot: '*[⚙️] 𝐸𝑠𝑡𝑒 𝑐𝑜𝑚𝑎𝑛𝑑𝑜 𝑠𝑜𝑙𝑜 𝑝𝑢𝑒𝑑𝑒 𝑠𝑒𝑟 𝑢𝑡𝑖𝑙𝑖𝑧𝑎𝑑𝑜 𝑝𝑜𝑟 𝑒𝑙 𝑏𝑜𝑡 𝑦 𝑒𝑙 𝑐𝑟𝑒𝑎𝑑𝑜𝑟!*',
+    wait: '*[⏳] 𝑃𝑜𝑟 𝑓𝑎𝑣𝑜𝑟 𝑒𝑠𝑝𝑒𝑟𝑒 𝑢𝑛 𝑚𝑜𝑚𝑒𝑛𝑡𝑜...*',
+    linkm: '*[❗] 𝐹𝑎𝑙𝑡𝑎 𝑒𝑙 𝑙𝑖𝑛𝑘!*',
+    error: '*[⚙️] 𝐸𝑟𝑟𝑜𝑟*',
+    endLimit: '*[❗] 𝑆𝑢 𝑙𝑖𝑚𝑖𝑡𝑒 𝑑𝑖𝑎𝑟𝑖𝑜 𝑠𝑒 𝑎 𝑎𝑔𝑜𝑡𝑎𝑑𝑜 𝑒𝑠𝑝𝑒𝑟𝑒 𝑎𝑙 𝑑𝑖𝑎 𝑠𝑖𝑔𝑢𝑢𝑒𝑛𝑡𝑒 𝑝𝑎𝑟𝑎 𝑞𝑢𝑒 𝑠𝑒 𝑟𝑒𝑖𝑛𝑖𝑐𝑖𝑒*',
+    ban: '*[❗] 𝐸𝑠𝑡𝑎𝑠 𝑏𝑎𝑛𝑒𝑎𝑑𝑜 ℎ𝑎𝑏𝑙𝑎 𝑐𝑜𝑛 𝑚𝑖 𝑐𝑟𝑒𝑎𝑑𝑜𝑟 𝑝𝑎𝑟𝑎 𝑡𝑢 𝑑𝑒𝑠𝑏𝑎𝑛𝑒𝑜*',
+    nsfw: '*[⚙️] 𝐿𝑎 𝑓𝑢𝑛𝑐𝑖𝑜𝑛 𝑁𝐹𝑆𝑊 𝑒𝑠𝑡𝑎 𝑑𝑒𝑠𝑎𝑐𝑡𝑖𝑣𝑎𝑑𝑜 ℎ𝑎𝑏𝑙𝑎 𝑐𝑜𝑛 𝑒𝑙 𝑎𝑑𝑚𝑖𝑛 𝑝𝑎𝑟𝑎 𝑎𝑐𝑡𝑖𝑣𝑎𝑟𝑙𝑜*',
+    banChat: '*[❗] 𝐸𝑙 𝑔𝑟𝑢𝑝𝑜 𝑒𝑠𝑡𝑎 𝑏𝑎𝑛𝑒𝑎𝑑𝑜 ℎ𝑎𝑏𝑙𝑎 𝑐𝑜𝑛 𝑒𝑙 𝑝𝑟𝑜𝑝𝑖𝑒𝑡𝑎𝑟𝑖𝑜 𝑝𝑎𝑟𝑎 𝑒𝑙 𝑑𝑒𝑠𝑏𝑎𝑛𝑒𝑜*'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 500,
+    monayawal: 200
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./Media/theme/bot.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`𝑆𝑒 𝑎𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑜 ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
